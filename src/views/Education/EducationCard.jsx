78f8d656c3a4201f919e460a84f5a50daa1fdfd8
Dennis Yau <>
2024-08-15 02:18:19 -0700

    regular

--- src/views/Education/EducationCard.jsx
+++ src/views/Education/EducationCard.jsx
@@ -31,62 +31,65 @@ function EducationCard({ content, index }) {
           <p className="card-text"></p>
         </div>
 
-        <ul className="list-group list-group-flush z-0 flex flex-wrap ">
-          {content.title && (
-            <li className="list-group-item z-0 font-bold text-xl bg-gray-400">
-              <span className={styles.label}>{content.title}</span>
-            </li>
-          )}
+        <div className="bg-gray-200">
+          <ul className="list-group list-group-flush z-0 flex flex-wrap  ">
+            {content.title && (
+              <li className="list-group-item z-0 font-bold text-xl bg-gray-500">
+                <span className="text-white">{content.title}</span>
+              </li>
+            )}
 
-          {content.ranking ? (
+            {content.ranking ? (
+              <li className="list-group-item z-0">
+                <span className={styles.label}>
+                  Times World Higher Education Ranking:
+                  <br />
+                </span>
+                <span>{content.ranking}</span>
+              </li>
+            ) : null}
+
+            {content.education_level ? (
+              <li className="list-group-item z-0">
+                <span className={styles.label}>Education level: </span>
+                <span>{content.education_level}</span>
+              </li>
+            ) : null}
+
+            {content.major ? (
+              <li className="list-group-item z-0">
+                <span className={styles.label}>Major: </span>
+                <span>{content.major}</span>
+              </li>
+            ) : null}
+
             <li className="list-group-item z-0">
-              <span className={styles.label}>
-                Times World Higher Education Ranking:
-                <br />
-              </span>
-              <span>{content.ranking}</span>
+              <span className={styles.label}>Location: </span>
+              <span>{content.location}</span>
             </li>
-          ) : null}
 
-          {content.education_level ? (
             <li className="list-group-item z-0">
-              <span className={styles.label}>Education level: </span>
-              <span>{content.education_level}</span>
+              <span className={styles.label}>Year: </span>
+              <span>{content.year}</span>
             </li>
-          ) : null}
 
-          {content.major ? (
-            <li className="list-group-item z-0">
-              <span className={styles.label}>Major: </span>
-              <span>{content.major}</span>
-            </li>
-          ) : null}
+            {content.honor ? (
+              <li className="list-group-item z-0">
+                <span className={styles.label}>Honor: </span>
+                <span>{content.honor}</span>
+              </li>
+            ) : null}
 
-          <li className="list-group-item z-0">
-            <span className={styles.label}>Location: </span>
-            <span>{content.location}</span>
-          </li>
+            {content.acheivement ? (
+              <li className="list-group-item z-0">
+                <span className={styles.label}>Acheivement: </span>
+                <span>{content.acheivement}</span>
+              </li>
+            ) : null}
+          </ul>
+        </div>
 
-          <li className="list-group-item z-0">
-            <span className={styles.label}>Year: </span>
-            <span>{content.year}</span>
-          </li>
-
-          {content.honor ? (
-            <li className="list-group-item z-0">
-              <span className={styles.label}>Honor: </span>
-              <span>{content.honor}</span>
-            </li>
-          ) : null}
-
-          {content.acheivement ? (
-            <li className="list-group-item z-0">
-              <span className={styles.label}>Acheivement: </span>
-              <span>{content.acheivement}</span>
-            </li>
-          ) : null}
-        </ul>
-        {content.cert ? (
+        {/* {content.cert ? (
           <div>
             <img
               src={content.cert}
@@ -94,7 +97,7 @@ function EducationCard({ content, index }) {
               className="w-full hidden"
             />
           </div>
-        ) : null}
+        ) : null} */}
 
         <div className=" flex flex-wrap z-0 gap-3 ">
           <Link
