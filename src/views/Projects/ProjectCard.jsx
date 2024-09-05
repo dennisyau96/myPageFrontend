7f236b130eeba1390802d102070547f1b51ca52d
Dennis Yau <>
2024-09-05 15:52:49 -0700

    add profile page photo

--- src/views/Projects/ProjectCard.jsx
+++ src/views/Projects/ProjectCard.jsx
@@ -12,11 +12,13 @@ export default function ProjectCard({ project }) {
     <>
       <div className="card w-80 h-auto  m-4 border-2 rounded-xl hover:  hover:transition-all hover:duration-300 ease-in-out  hover:shadow-2xl hover:shadow-sky-400 hover:bg-slate-100 opacity-95">
         {project.thumbnail && (
-          <img
-            src={project.thumbnail}
-            className="card-img-top rounded-xl bg-gray-950 text-white"
-            alt={project.thumbnail}
-          />
+          <a href={project.thumbnail}>
+            <img
+              src={project.thumbnail}
+              className="card-img-top rounded-xl bg-gray-950 text-white"
+              alt={project.thumbnail}
+            />
+          </a>
         )}
         <div className="card-body text-center">
           <h5 className="card-title font-bold text-xl mb-2 text-center">
