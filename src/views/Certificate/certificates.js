45c838f557d3f59aad24c83262bedfd60b5d585c
Dennis Yau <>
2024-10-02 22:35:44 -0700

    gallery

---
+++ src/views/Certificate/certificates.js
@@ -0,0 +1,178 @@
+import cert1 from "../../assets/certificates/cert1.jpg";
+import cert2 from "../../assets/certificates/cert2.jpg";
+import cert3 from "../../assets/certificates/cert3.jpg";
+import cert4 from "../../assets/certificates/cert4.jpg";
+import cert5 from "../../assets/certificates/cert5.jpg";
+import cert6 from "../../assets/certificates/cert6.jpg";
+import cert7 from "../../assets/certificates/cert7.jpg";
+import cert8 from "../../assets/certificates/cert8.jpg";
+import cert9 from "../../assets/certificates/cert9.jpg";
+import cert10 from "../../assets/certificates/cert10.jpg";
+import cert11 from "../../assets/certificates/cert11.jpg";
+import cert12 from "../../assets/certificates/cert12.jpg";
+import cert13 from "../../assets/certificates/cert13.jpg";
+import cert14 from "../../assets/certificates/cert14.jpg";
+import cert15 from "../../assets/certificates/cert15.jpg";
+import cert16 from "../../assets/certificates/cert16.jpg";
+import cert17 from "../../assets/certificates/cert17.jpg";
+import cert18 from "../../assets/certificates/cert18.jpg";
+import cert19 from "../../assets/certificates/cert19.jpg";
+import cert20 from "../../assets/certificates/cert20.jpg";
+import cert21 from "../../assets/certificates/cert21.jpg";
+import cert22 from "../../assets/certificates/cert22.jpg";
+import cert23 from "../../assets/certificates/cert23.jpg";
+import cert24 from "../../assets/certificates/cert24.jpg";
+import cert25 from "../../assets/certificates/cert25.jpg";
+import cert26 from "../../assets/certificates/cert26.jpg";
+import cert27 from "../../assets/certificates/cert27.jpg";
+import cert28 from "../../assets/certificates/cert28.jpg";
+import cert29 from "../../assets/certificates/cert29.jpg";
+
+export const certs = [
+  {
+    cert: cert1,
+    title: "Learning Node.js",
+    link: "src/assets/certificates/pdf/cert1.pdf",
+  },
+  {
+    cert: cert2,
+    title: "MERN Essential Training",
+    link: "src/assets/certificates/pdf/cert2.pdf",
+  },
+  {
+    cert: cert3,
+    title: "Node.js Essential Training",
+    link: "src/assets/certificates/pdf/cert3.pdf",
+  },
+  {
+    cert: cert4,
+    title: "Node.js: Web Servers, Tests, and Deployment",
+    link: "src/assets/certificates/pdf/cert4.pdf",
+  },
+  {
+    cert: cert5,
+    title: "React: Working with APIs",
+    link: "src/assets/certificates/pdf/cert5.pdf",
+  },
+  {
+    cert: cert6,
+    title: "React.js Essential Training",
+    link: "src/assets/certificates/pdf/cert6.pdf",
+  },
+  {
+    cert: cert7,
+    title: "Succeeding in Web Development: Full Stack and Front End",
+    link: "src/assets/certificates/pdf/cert7.pdf",
+  },
+  {
+    cert: cert8,
+    title: "Web Programming Foundations",
+    link: "src/assets/certificates/pdf/cert8.pdf",
+  },
+  {
+    cert: cert9,
+    title: "Introduction to Generative AI",
+    link: "src/assets/certificates/pdf/cert9.pdf",
+  },
+  {
+    cert: cert10,
+    title: "Build a free website with WordPress",
+    link: "src/assets/certificates/pdf/cert10.pdf",
+  },
+  {
+    cert: cert11,
+    title: "Express Essentials: Build Powerful Web Apps with Node.js",
+    link: "src/assets/certificates/pdf/cert11.pdf",
+  },
+  {
+    cert: cert12,
+    title: "Fundamentals of digital marketing",
+    link: "src/assets/certificates/pdf/cert12.pdf",
+  },
+  {
+    cert: cert13,
+    title: "Java 8 Essential Training",
+    link: "src/assets/certificates/pdf/cert13.pdf",
+  },
+  {
+    cert: cert14,
+    title: "Java Essential Training: Objects and APIs",
+    link: "src/assets/certificates/pdf/cert14.pdf",
+  },
+  {
+    cert: cert15,
+    title: "Java Essential Training: Syntax and Structure",
+    link: "src/assets/certificates/pdf/cert15.pdf",
+  },
+  {
+    cert: cert16,
+    title: "JavaScript Essential Training",
+    link: "src/assets/certificates/pdf/cert16.pdf",
+  },
+  {
+    cert: cert17,
+    title: "JavaScript for Web Designers",
+    link: "src/assets/certificates/pdf/cert17.pdf",
+  },
+  {
+    cert: cert18,
+    title: "Learning Java 17",
+    link: "src/assets/certificates/pdf/cert18.pdf",
+  },
+  {
+    cert: cert19,
+    title: "学习 SQL (Learning SQL)",
+    link: "src/assets/certificates/pdf/cert19.pdf",
+  },
+  {
+    cert: cert20,
+    title: "Adobe Illustrator CC",
+    link: "src/assets/certificates/pdf/cert20.pdf",
+  },
+  {
+    cert: cert21,
+    title: "Git Essential Training",
+    link: "src/assets/certificates/pdf/cert21.pdf",
+  },
+  {
+    cert: cert22,
+    title: "Kotlin Essential Training",
+    link: "src/assets/certificates/pdf/cert22.pdf",
+  },
+  {
+    cert: cert23,
+    title: "Java: Testing with JUnit",
+    link: "src/assets/certificates/pdf/cert23.pdf",
+  },
+  {
+    cert: cert24,
+    title: "IT Help Desk for Beginners",
+    link: "src/assets/certificates/pdf/cert24.pdf",
+  },
+  {
+    cert: cert25,
+    title:
+      "SQL Essential Training (National Association of State Boards of Accountancy)",
+    link: "src/assets/certificates/pdf/cert25.pdf",
+  },
+  {
+    cert: cert26,
+    title: "Advanced CSS Media Queries",
+    link: "src/assets/certificates/pdf/cert26.pdf",
+  },
+  {
+    cert: cert27,
+    title: "Advanced Node.js: Scaling Applications",
+    link: "src/assets/certificates/pdf/cert27.pdf",
+  },
+  {
+    cert: cert28,
+    title: "Using MongoDB with Node.js",
+    link: "src/assets/certificates/pdf/cert28.pdf",
+  },
+  {
+    cert: cert29,
+    title: "Building RESTful APIs with Node.js and Express",
+    link: "src/assets/certificates/pdf/cert29.pdf",
+  },
+];
