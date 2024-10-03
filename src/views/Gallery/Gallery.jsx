9e344525447040c4b0bfdd055a469481a596cff6
Dennis Yau <>
2024-10-02 21:33:38 -0700

    for deploy

--- src/views/Gallery/Gallery.jsx
+++ src/views/Gallery/Gallery.jsx
@@ -3,27 +3,44 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import qr from "../../../src/assets/gallery/QR.jpg";
 import pic1 from "../../../src/assets/gallery/pic1.jpg";
-import pic2 from "../../../src/assets/gallery/pic2.jpg";
-import pic3 from "../../../src/assets/gallery/pic3.jpg";
+// import pic2 from "../../../src/assets/gallery/pic2.jpg";
+// import pic3 from "../../../src/assets/gallery/pic3.jpg";
 import pic4 from "../../../src/assets/gallery/pic4.jpg";
 import pic5 from "../../../src/assets/gallery/pic5.jpg";
-import pic6 from "../../../src/assets/gallery/pic6.jpg";
-import pic7 from "../../../src/assets/gallery/pic7.jpg";
+// import pic6 from "../../../src/assets/gallery/pic6.jpg";
+// import pic7 from "../../../src/assets/gallery/pic7.jpg";
 import pic8 from "../../../src/assets/gallery/pic8.jpg";
 import pic9 from "../../../src/assets/gallery/pic9.jpg";
+import pic10 from "../../../src/assets/gallery/pic10.jpg";
+import pic11 from "../../../src/assets/gallery/pic11.jpg";
+import pic12 from "../../../src/assets/gallery/pic12.jpg";
+import pic13 from "../../../src/assets/gallery/pic13.jpg";
+import pic14 from "../../../src/assets/gallery/pic14.jpg";
+import pic15 from "../../../src/assets/gallery/pic15.jpg";
+import pic16 from "../../../src/assets/gallery/pic16.jpg";
+// import pic18 from "../../../src/assets/gallery/pic8.jpg";
+// import pic19 from "../../../src/assets/gallery/pic9.jpg";
+
 import { useNavigate } from "react-router-dom";
 export default function Gallery() {
   const navigate = useNavigate();
   const photosSrc = [
     { href: "../../../src/assets/gallery/pic1.jpg", pic: pic1 },
-    { href: "../../../src/assets/gallery/pic2.jpg", pic: pic2 },
-    { href: "../../../src/assets/gallery/pic3.jpg", pic: pic3 },
+    // { href: "../../../src/assets/gallery/pic2.jpg", pic: pic2 },
+    // { href: "../../../src/assets/gallery/pic3.jpg", pic: pic3 },
     { href: "../../../src/assets/gallery/pic4.jpg", pic: pic4 },
     { href: "../../../src/assets/gallery/pic5.jpg", pic: pic5 },
-    { href: "../../../src/assets/gallery/pic6.jpg", pic: pic6 },
-    { href: "../../../src/assets/gallery/pic7.jpg", pic: pic7 },
+    // { href: "../../../src/assets/gallery/pic6.jpg", pic: pic6 },
+    // { href: "../../../src/assets/gallery/pic7.jpg", pic: pic7 },
     { href: "../../../src/assets/gallery/pic8.jpg", pic: pic8 },
     { href: "../../../src/assets/gallery/pic9.jpg", pic: pic9 },
+    { href: "../../../src/assets/gallery/pic10.jpg", pic: pic10 },
+    { href: "../../../src/assets/gallery/pic11.jpg", pic: pic11 },
+    { href: "../../../src/assets/gallery/pic12.jpg", pic: pic12 },
+    { href: "../../../src/assets/gallery/pic13.jpg", pic: pic13 },
+    { href: "../../../src/assets/gallery/pic14.jpg", pic: pic14 },
+    { href: "../../../src/assets/gallery/pic15.jpg", pic: pic15 },
+    { href: "../../../src/assets/gallery/pic16.jpg", pic: pic16 },
   ];
 
   const [index, setIndex] = useState(0);
@@ -56,7 +73,7 @@ export default function Gallery() {
         {/*carousel */}
         <div
           id="carouselExampleAutoplaying"
-          className="carousel slide  carousel-fade w-8/12  my-4 text-center justify-center border-2 border-gray-300 py-10 hover:shadow-sky-300 hover:shadow-md duration-500 ease-in-out transition-all"
+          className="carousel slide  carousel-fade w-6/12  my-4 text-center justify-center border-2 border-gray-300 py-10 hover:shadow-sky-300 hover:shadow-md duration-500 ease-in-out transition-all max-h-1/2"
           data-bs-ride="carousel"
           data-ride="carousel"
           data-pause="false"
